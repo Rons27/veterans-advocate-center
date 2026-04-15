a73b74cb44c5e8d35ae8f4ae110e71eb8ca0159c
Ronnie B Alfonso Jr <ronnie@example.com>
2026-04-15 11:29:26 +0800

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/veterans-advocate-center/',
   server: {
     port: 3000,
     open: true,
